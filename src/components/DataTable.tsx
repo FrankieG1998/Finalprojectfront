d3880025c33e7c85b44f3a852d6033279db5bba7
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-11-20 13:57:03 -0400

    Update DataTable.tsx

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,25 +1,40 @@
-import Button from "./Button"
-import Modal from "./Modal"
-import {useState} from "react";
-
-import { server_calls } from "../api/server";
+import React, { useState, useEffect } from 'react'; // Import useEffect
+import Button from "./Button";
+import Modal from "./Modal";
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { useGetData } from "../custom-hooks/FetchData";
-
+import { getAuth } from 'firebase/auth'; // Import Firebase Authentication
 
 const columns: GridColDef[] = [
-    {field: 'id', headerName: "ID" }, //hide: true 
-    {field: 'image_title', headerName: 'Image Title', flex: 1}, 
-    {field: 'creator_name', headerName: 'Creator Name', flex: 1},
-    {field: 'image_type', headerName: 'Image Type', flex: 1},
-    {field: 'image_url', headerName: 'Image URL', flex: 1}
-]
+    { field: 'id', headerName: "ID" }, // consider using 'hide: true' if you don't want to display the ID
+    { field: 'image_title', headerName: 'Image Title', flex: 1 },
+    { field: 'creator_name', headerName: 'Creator Name', flex: 1 },
+    { field: 'image_type', headerName: 'Image Type', flex: 1 },
+    { field: 'image_url', headerName: 'Image URL', flex: 1 }
+];
 
 function DataTable() {
     const [open, setOpen] = useState(false);
-    const {imageData, getData} = useGetData();
-    const [ selectionModel, setSelectionModel] = useState<string[]>([])
+    const [imageData, setImageData] = useState([]); // State to hold image data
+    const [selectionModel, setSelectionModel] = useState<string[]>([]);
+    const auth = getAuth(); // Initialize Firebase Auth
 
+    useEffect(() => {
+        // Function to fetch data for the current user
+        const fetchData = async () => {
+            if (auth.currentUser) {
+                const userId = auth.currentUser.uid;
+                try {
+                    const response = await server_calls.getUserImages(userId); // A hypothetical function to fetch user-specific images
+                    setImageData(response.data);
+                } catch (error) {
+                    console.error("Error fetching data: ", error);
+                }
+            }
+        };
+
+        fetchData();
+    }, [auth.currentUser]); // Effect runs when the currentUser changes
+    
     const handleOpen = () =>{
         setOpen(true)
     }
