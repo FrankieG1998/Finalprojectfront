5a6d545b42d6551b89a4828ea024ec797f728489
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-11-20 12:09:20 -0400

    Update ImageForm.tsx

--- src/components/ImageForm.tsx
+++ src/components/ImageForm.tsx
@@ -5,6 +5,7 @@ import {useForm} from 'react-hook-form';
 import {server_calls} from '../api/server';
 import { useDispatch, useStore } from 'react-redux';
 import { chooseImageTitle, chooseCreatorName, chooseImageType, chooseImageUrl } from '../redux/slices/RootSlice';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 //interfaces
 interface ImageFormProps {
@@ -22,6 +23,18 @@ const ImageForm = (props:ImageFormProps) => {
     console.log(`ID: ${props.id}`);
     console.log(data);
 
+    if (selectedFile) {
+      const storage = getStorage();
+      const storageRef = ref(storage, 'images/' + selectedFile.name);
+  
+      // Upload the file to Firebase Storage
+      const uploadResult = await uploadBytes(storageRef, selectedFile);
+      // Get the URL of the uploaded file
+      const imageUrl = await getDownloadURL(uploadResult.ref);
+  
+      // Add the image URL to the form data
+      data.image_url = imageUrl;
+    
     if (props.id && props.id.length >0){
       server_calls.update(props.id[0], data)
       console.log(`updated: ${data.image_title} ${props.id}`)
@@ -60,11 +73,12 @@ const ImageForm = (props:ImageFormProps) => {
           <label htmlFor="image_type"> Image Type</label>
             <Input {...register('image_type')} name="image_type" placeholder="Image Type" />
         </div>
+        <input type="file" onChange={handleFileChange} />
         <div className="flex p-1">
           <Button className='flex justify-center m-3 bg-slate-300 p-2  rounded hover:bg-slate-800 text-white'
           > Submit </Button>
         </div>
-
+      
       </form>
     </div>
   )
