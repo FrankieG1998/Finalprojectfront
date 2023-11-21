c12748e13b9f58c26c1b5f26d194922ab73f442c
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-11-21 09:04:05 -0400

    Update Gallery.tsx

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,14 +1,10 @@
-import SimpleImageSlider from "react-simple-image-slider";
 import { useState, useEffect } from 'react';
+import SimpleImageSlider from "react-simple-image-slider";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
 
-type SliderImage = {
-    url: string;
-};
-
 function Gallery() {
-    const [sliderImages, setSliderImages] = useState([]);
+    const [sliderImages, setSliderImages] = useState<{ url: string; }[]>([]); // Explicitly type the state
     const auth = getAuth();
     const storage = getStorage();
 
@@ -19,39 +15,43 @@ function Gallery() {
                 listAll(userFolderRef)
                     .then((res) => {
                         const imageRefs = res.items;
-                        return Promise.all(imageRefs.map((imageRef) => 
+                        return Promise.all(imageRefs.map((imageRef) =>
                             getDownloadURL(imageRef).then((url) => ({ url }))
                         ));
                     })
                     .then((images) => {
-                        setSliderImages(images);
+                        setSliderImages(images); // Set the fetched URLs in the state
                     })
                     .catch((error) => {
                         console.error("Error fetching images: ", error);
                     });
+            } else {
+                // Handle the case where there is no user signed in
+                setSliderImages([]);
             }
         });
     }, [auth, storage]);
-    console.log(sliderImages);
 
     return (
-<div>
+        <div>
             <h3 className="flex justify-center m-2">Welcome to My Image Gallery</h3>
             <div className="flex justify-center">
-                {sliderImages.length > 0 && (
+                {sliderImages.length > 0 ? (
                     <SimpleImageSlider
-                        width='80%'
-                        height='80%'
+                        width={896} // You can use numerical values for width and height
+                        height={504}
                         images={sliderImages}
                         showNavs={true}
                         showBullets={false}
                         autoPlay={true}
                         autoPlayDelay={2.0}
                     />
+                ) : (
+                    <p>Loading images...</p> // Display a loading message or a spinner
                 )}
             </div>
         </div>
-    )
+    );
 }
 
 export default Gallery;
