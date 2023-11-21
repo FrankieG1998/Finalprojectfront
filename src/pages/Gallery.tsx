d54abf683574de875f0a3f7933ce394c35b8a28c
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-11-21 08:44:49 -0400

    Update Gallery.tsx

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,42 +1,52 @@
 import SimpleImageSlider from "react-simple-image-slider";
-import { useGetData } from "../custom-hooks/FetchData";
+import { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
 
-function gallery() {
-  //const {imageData, getData} = useGetData();
-  const {imageData} = useGetData();
-//   type objType = {
-//     url: string;
-//  };
+function Gallery() {
+    const [sliderImages, setSliderImages] = useState([]);
+    const auth = getAuth();
+    const storage = getStorage();
 
-  //let sliderImages: Array<objType> = [];
-  
-  const sliderImages = [{
-        url: "https://content.api.news/v3/images/bin/f3e024e0f1063afd90e1c94664862e10?width=768",
-     },];
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const userFolderRef = ref(storage, `images/${user.uid}`);
+                listAll(userFolderRef)
+                    .then((res) => {
+                        const imageRefs = res.items;
+                        return Promise.all(imageRefs.map((imageRef) => 
+                            getDownloadURL(imageRef).then((url) => ({ url }))
+                        ));
+                    })
+                    .then((images) => {
+                        setSliderImages(images);
+                    })
+                    .catch((error) => {
+                        console.error("Error fetching images: ", error);
+                    });
+            }
+        });
+    }, [auth, storage]);
 
-  for (let i=0; i<imageData.length; i++){
-    sliderImages.push({'url': imageData[i]['image_url']});
-  }
-  //console.log(sliderImages);
- 
-  return (
-    <div>
-      <h3 className="flex justify-center m-2">
-        {" "}
-          Welcome to My Image Gallery 
-      </h3>
-    <div className="flex justify-center">
-    <SimpleImageSlider
-        width= '80%'
-        height='80%'
-        images={sliderImages}
-        showNavs={true} showBullets={false} 
-        autoPlay={true} 
-        autoPlayDelay = {1.0}
-          />
-    </div>
- </div>
-  )
+    return (
+        <div>
+            <h3 className="flex justify-center m-2">
+                Welcome to My Image Gallery
+            </h3>
+            <div className="flex justify-center">
+                <SimpleImageSlider
+                    width='80%'
+                    height='80%'
+                    images={sliderImages}
+                    showNavs={true}
+                    showBullets={false}
+                    autoPlay={true}
+                    autoPlayDelay={1.0}
+                />
+            </div>
+        </div>
+    )
 }
 
-export default gallery
+export default Gallery;
