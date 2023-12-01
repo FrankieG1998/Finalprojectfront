a78e3fed590c1cf601cad2c991940f9bd120db70
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-12-01 16:24:26 -0500

    Update RootSlice.ts

--- src/redux/slices/RootSlice.ts
+++ src/redux/slices/RootSlice.ts
@@ -3,14 +3,14 @@ import {createSlice} from "@reduxjs/toolkit"
 const rootSlice = createSlice({
     name: "root", 
     initialState: {
-        image_title: "Image Title",
+        name: "Name",
         creator_name: "Creator Name",
         image_url: "Image URL",
         image_type: "Image Type",
     },
     reducers:{
         //action is submitted eleswhere - written to state.name
-        chooseImageTitle: (state, action) => {state.image_title = action.payload}, //We are setting the input to the state.name
+        chooseName: (state, action) => {state.name = action.payload}, //We are setting the input to the state.name
         chooseCreatorName: (state, action) => {state.creator_name = action.payload},
         chooseImageUrl: (state, action) => {state.image_url = action.payload},
         chooseImageType: (state, action) => {state.image_type = action.payload},
@@ -18,4 +18,4 @@ const rootSlice = createSlice({
 })
 
 export const reducer = rootSlice.reducer;
-export const {chooseImageTitle, chooseCreatorName, chooseImageUrl, chooseImageType} = rootSlice.actions
+export const {chooseName, chooseCreatorName, chooseImageUrl, chooseImageType} = rootSlice.actions
